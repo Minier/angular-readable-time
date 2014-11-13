25dffc6db568b0f3f98d1fde55b7213e905f2bd9
Minier <nathaniel.minier.mil@mail.mil>
2014-11-13 11:21:23 -0500

    Update angular-readable-time.js
    Gives an extended format for an exact duration.

--- angular-readable-time.js
+++ angular-readable-time.js
@@ -1,38 +1,55 @@
 (function() {
   'use strict';
-  angular.module('readableTime', []).filter('readableTime', function() {
-    return function(seconds) {
-      var day, format, hour, minute, month, week, year;
-      seconds = parseInt(seconds, 10);
-      minute = 60;
-      hour = minute * 60;
-      day = hour * 24;
-      week = day * 7;
-      year = day * 365;
-      month = year / 12;
-      format = function(number, string) {
-        string = number === 1 ? string : "" + string + "s";
-        return "" + number + " " + string;
-      };
-      switch (false) {
-        case !(seconds < minute):
-          return format(seconds, 'second');
-        case !(seconds < hour):
-          return format(Math.floor(seconds / minute), 'minute');
-        case !(seconds < day):
-          return format(Math.floor(seconds / hour), 'hour');
-        case !(seconds < week):
-          return format(Math.floor(seconds / day), 'day');
-        case !(seconds < month):
-          return format(Math.floor(seconds / week), 'week');
-        case !(seconds < year):
-          return format(Math.floor(seconds / month), 'month');
-        default:
-          return format(Math.floor(seconds / year), 'year');
-      }
-    };
-  });
+  angular.module('readableTime', []).filter('readableTime', function () {
+    return function (seconds) {
+        var format, parse, day, hour, minute, week, month, year, output;
+        seconds = parseInt(seconds, 10);
+        minute = 60;
+        hour = minute * 60;
+        day = hour * 24;
+        week = day * 7;
+        year = week * 52;
+        month = year / 12;
+        output = "";
+        format = function (number, string) {
+            string = number === 1 ? string : "" + string + "s";
+            return "" + number + " " + string + " ";
+        };
 
+        parse = function (toParse) {
+            switch (true) {
+                case (toParse > year):
+                    output += format(Math.floor(toParse / year), 'year');
+                    parse(toParse % year);
+                    break;
+                case (toParse > month):
+                    output += format(Math.floor(toParse / month), 'month');
+                    parse(toParse % month);
+                    break;
+                case (toParse > week):
+                    output += format(Math.floor(toParse / week), 'week');
+                    parse(toParse % week);
+                    break;
+                case (toParse > day):
+                    output += format(Math.floor(toParse / day), 'day');
+                    parse(toParse % day);
+                    break;
+                case (toParse > hour):
+                    output += format(Math.floor(toParse / hour), 'hour');
+                    parse(toParse % hour);
+                    break;
+                case (toParse > minute):
+                    output += format(Math.floor(toParse / minute), 'minute');
+                    parse(toParse % minute);
+                    break;
+                default:
+                    output += format(toParse, 'second');
+            }
+        };
+
+        parse(seconds);
+        return output;
+    };
 }).call(this);
 
 /*
